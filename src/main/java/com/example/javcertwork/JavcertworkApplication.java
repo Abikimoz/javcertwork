565e6a5de6edfd68e04bcf0dc859a5eb3624c400
Abikimoz <i.m.anisimov25@gmail.com>
2024-12-19 22:15:03 +0300

    Initial commit

---
+++ src/main/java/com/example/javcertwork/JavcertworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.javcertwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavcertworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavcertworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javcertwork/JavcertworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javcertwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavcertworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
